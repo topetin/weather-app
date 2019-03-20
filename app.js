9ac1b0f6a2039495108210e280eee13890b1b30f
topetin <carballalmariana@gmail.com>
2019-03-20 17:22:09 -0300

    forecast and geocode moved to utils directory, app working and handling errors

--- app.js
+++ app.js
@@ -1,15 +1,19 @@
-const request = require('request');
-const geocode = require('./geocode.js');
-
-// const url = 'https://api.darksky.net/forecast/32e45e6c41a044618f9d3b75a2113b08/37.8267,-122.4233'
-
-// request({url: url, json: true}, function(error, response, body) {
-//     console.log(`${body.daily.data[0].summary} It's currently ${body.currently.temperature} degrees out. There is a ${body.currently.precipProbability}% chance of rain)`)
-// });
+const request = require('request')
+const geocode = require('./utils/geocode.js')
+const forecast = require('./utils/forecast.js')
 
 geocode('Buenos Aires', (error, data) => {
-    console.log('Error', error);
-    console.log('Data', data);
-        
+    try {
+        console.log(data.location)
+        forecast(data.latitude, data.longitude, (forecastError, forecastData) => {
+            if (forecastError) {
+                console.log(forecastError)
+            } else {
+                console.log(forecastData)
+            }
+        })
+    } 
+    catch (e) {
+        console.log(error)
+    }
 })
-
